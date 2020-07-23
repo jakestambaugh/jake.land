76c662da669342abde9eae288d1d9db85f76a06b
Jake Stambaugh <jake.stambaugh@gmail.com>
2020-07-22 20:13:35 -0400

    Handle get requests to show a specific post

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,51 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn home() -> &'static str {
     "Hello, world! Welcome to Jake's blog"
 }
 
+#[get("/<post_id>")]
+fn show_post(post_id: u64) -> String {
+    format!("Post id: {}", post_id)
+}
+
 #[launch]
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![home])
+    rocket::ignite()
+        .mount("/", routes![home])
+        .mount("/post/", routes![show_post])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+
+    #[test]
+    fn test_post_31() {
+        use rocket::local::blocking::Client;
+
+        let client = Client::new(rocket()).unwrap();
+        let response = client.get("/post/31").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(
+            response.into_string(),
+            Some("Post id: 31".into())
+        )
+    }
+
+    #[test]
+    fn test_home() {
+        use rocket::local::blocking::Client;
+
+        let client = Client::new(rocket()).unwrap();
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(
+            response.into_string(),
+            Some("Hello, world! Welcome to Jake's blog".into())
+        )
+    }
 }
